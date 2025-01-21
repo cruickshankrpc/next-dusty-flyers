32eacd690e6ad0ac5271f37ddcbd1c29d35ebf1a
Raquel <cruickshankrpc@gmail.com>
2025-01-21 18:37:21 +1300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,58 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import localFont from 'next/font/local';
+import instagramIcon from "./images/instagram.png";
+import emailIcon from "./images/email.png";
+import facebookIcon from "./images/facebook.png";
+import Link from "next/link";
+
+const chantalFont = localFont({
+  src: './fonts/Chantal.ttf',
+})
 
 export default function Home() {
+ 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <div className={styles.content}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/logo.png"
+          alt="Dusty Flyer's logo"
+          width={320}
+          height={255}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <h2 className={`${chantalFont.className} ${styles.subtitle}`}>The ride of your life, guaranteed</h2>
+        <div className={styles.iconContainer}>
+            <Link
+              href="https://www.instagram.com/dustyflyersclub/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image className={styles.icon} src={instagramIcon} alt="Instagram Icon" />
+            </Link>
+            <Link
+              href="https://www.facebook.com/dustyflyersclub/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image className={styles.icon}  src={facebookIcon} alt="Facebook Icon" />
+            </Link>
+            <Link
+              href="mailto:info@dustyflyersclub.club"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image className={styles.emailIcon}  src={emailIcon} alt="Email Icon" />
+            </Link>
+          </div>
         </div>
+      <div className={styles.bgImg}>
+    </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
   );
